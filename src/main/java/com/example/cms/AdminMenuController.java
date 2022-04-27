117eaf27520cde698be3e1119571a29b25219a43
Rustam-Fozilov <fozilovrustam32@gmail.com>
2022-04-27 16:59:10 +0500

    admin profile is ready

--- src/main/java/com/example/cms/AdminMenuController.java
+++ src/main/java/com/example/cms/AdminMenuController.java
@@ -1,4 +1,104 @@
 package com.example.cms;
 
-public class AdminMenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    @FXML
+    private VBox vBoxMenuBar;
+
+    @FXML
+    private VBox vboxProfile;
+
+    @FXML
+    private Button btnManageCourse;
+
+    @FXML
+    private Button btnManageStudent;
+
+    @FXML
+    private Button btnMyProfile;
+
+    @FXML
+    private Rectangle rect1;
+
+    @FXML
+    private Rectangle rect2;
+
+    @FXML
+    private Rectangle rect3;
+
+    @FXML
+    private Circle avatar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setBlurType(BlurType.GAUSSIAN);
+        dropShadow.setColor(Color.rgb(230, 230, 230));
+        dropShadow.setHeight(20);
+        dropShadow.setWidth(20);
+        dropShadow.setOffsetX(2);
+        dropShadow.setOffsetY(0);
+        dropShadow.setRadius(25);
+        vBoxMenuBar.setEffect(dropShadow);
+
+        Image img = new Image("avatar.png");
+        avatar.setFill(new ImagePattern(img));
+
+        DropShadow dropShadow2 = new DropShadow();
+        dropShadow2.setBlurType(BlurType.GAUSSIAN);
+        dropShadow2.setColor(Color.rgb(170, 213, 255));
+        dropShadow2.setHeight(24);
+        dropShadow2.setWidth(24);
+        dropShadow2.setOffsetY(8);
+        dropShadow2.setRadius(22);
+        vboxProfile.setEffect(dropShadow2);
+    }
+
+    public void myProfile(ActionEvent actionEvent) {
+        rect1.setVisible(true);
+        rect2.setVisible(false);
+        rect3.setVisible(false);
+
+        btnMyProfile.setTextFill(Color.rgb(0, 155, 255));
+        btnManageCourse.setTextFill(Color.BLACK);
+        btnManageStudent.setTextFill(Color.BLACK);
+        btnMyProfile.setStyle("-fx-background-color: #fff");
+    }
+
+    public void manageCourse(ActionEvent actionEvent) {
+        rect2.setVisible(true);
+        rect1.setVisible(false);
+        rect3.setVisible(false);
+
+        btnManageCourse.setTextFill(Color.rgb(0, 155, 255));
+        btnManageStudent.setTextFill(Color.BLACK);
+        btnMyProfile.setTextFill(Color.BLACK);
+        btnManageCourse.setStyle("-fx-background-color: #fff");
+    }
+
+    public void manageStudent(ActionEvent actionEvent) {
+        rect3.setVisible(true);
+        rect2.setVisible(false);
+        rect1.setVisible(false);
+
+        btnManageStudent.setTextFill(Color.rgb(0, 155, 255));
+        btnManageCourse.setTextFill(Color.BLACK);
+        btnMyProfile.setTextFill(Color.BLACK);
+        btnManageStudent.setStyle("-fx-background-color: #fff");
+    }
 }
--- src/main/java/com/example/cms/LoginController.java
+++ src/main/java/com/example/cms/LoginController.java
@@ -5,20 +5,26 @@ import com.example.cms.JDBC.Database;
 import com.example.cms.JDBC.TeacherInformation;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
 
-public class LoginController {
+public class LoginController implements Initializable {
     @FXML
     private Label label;
 
@@ -78,5 +84,18 @@ public class LoginController {
             label.setText("username yoki parol xato :(");
             label.setStyle("-fx-text-fill: red");
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DropShadow dropShadow = new DropShadow();
+        dropShadow.setBlurType(BlurType.GAUSSIAN);
+        dropShadow.setColor(Color.rgb(179, 213, 255));
+        dropShadow.setHeight(20);
+        dropShadow.setWidth(20);
+        dropShadow.setOffsetX(0);
+        dropShadow.setOffsetY(10);
+        dropShadow.setRadius(21);
+        btnLogin.setEffect(dropShadow);
     }
 }
