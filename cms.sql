eac6ec8f8530df636775368fa16c6e4e4a8c1b14
Rustam-Fozilov <fozilovrustam32@gmail.com>
2022-05-08 00:35:50 +0500

    Added payment table

--- cms.sql
+++ cms.sql
@@ -6,27 +6,23 @@ select * from admins;
 select * from users;
 select * from teachers;
 select * from courses;
+select * from payments;
 
 
 create table admins(
 	AdminId int not null auto_increment,
-    Ismi varchar(255),
-    Familiyasi varchar(255),
+    FIO varchar(255),
     Telefon varchar(255),
-    Email varchar(255),
     username varchar(255),
     parol varchar(255),
     primary key (AdminId)
 );
 
-truncate table admins;
 
 create table users(
 	UserId int not null auto_increment,
-	Ismi varchar(255),
-    Familiyasi varchar(255),
+	FIO varchar(255),
     Telefon varchar(255),
-    Email varchar(255),
     Fani varchar(255),
     Guruhi varchar(255),
     primary key(UserId)
@@ -35,11 +31,10 @@ create table users(
 
 create table teachers(
 	TeacherId int not null auto_increment,
-	Ismi varchar(255),
-    Familiyasi varchar(255),
+	FIO varchar(255),
     Telefon varchar(255),
-    Email varchar(255),
     Fani varchar(255),
+    Guruhi varchar(255),
 	username varchar(255),
     parol varchar(255),
     primary key(TeacherId)
@@ -55,32 +50,47 @@ create table courses(
     primary key(CourseId)
 );
 
+create table payments(
+	PaymentId int not null auto_increment,
+    FIO varchar(255),
+    Fani varchar(255),
+    Miqdori int,
+    Oy varchar(255),
+    Sana date,
+    primary key(PaymentId)
+);
 
-create table groupss(
-	GroupId int not null auto_increment,
-    primary key(GroupId)
-); /* run qilinmagan */
 
+insert into payments(FIO, Fani, Miqdori, Oy, Sana)
+values 	('Akmal Umarov', 'IELTS', 200000, 'Mart', '2020-05-05');
 
-insert into admins(Ismi, Familiyasi, Telefon, Email, username, parol)
-values 	('Rustam', 'Fozilov', '+998977672097', 'rustamfozilov@gmail.com', 'Rustam19', 'RustamAdmin'),
-		('Mohir', 'Mahmudov', '+998999020352', 'mohirmahmudov@gmail.com', 'Mohir19', 'MohirAdmin');
+insert into admins(FIO, Telefon, username, parol)
+values 	('Rustam Fozilov', '+998977672097', 'Rustam19', 'RustamAdmin'),
+		('Mohir Mahmudov', '+998999020352', 'Mohir19', 'MohirAdmin');
         
 
-insert into users(Ismi, Familiyasi, Telefon, Email, Fani, Guruhi)
-values 	('Akmal', 'Umarov', '+998904450022', 'akmalumarov@gmail.com', 'Matematika', '1 - guruh'),
-		('Eldor', 'Sharipov', '+998991235476', 'eldorsharipov@gmail.com', 'Ingliz tili', '1 - guruh');
+insert into users(FIO, Telefon, Fani, Guruhi)
+values 	('Akmal Umarov', '+998904450022', 'Matematika', '1 - guruh'),
+		('Eldor Sharipov', '+998991235476', 'Ingliz tili', '1 - guruh');
 
 
 
-insert into teachers(Ismi, Familiyasi, Telefon, Email, Fani, username, parol)
-values 	('Davron', 'Ergashev', '+998901237898', 'davronergashev@gmail.com', 'Matematika', 'Davronbek', 'DavronE98'),
-		('Tohir', 'Hamidov', '+998993227899', 'tohirhamidov@gmail.com', 'Ingliz tili', 'Tohirbek', 'TohirH99');
+insert into teachers(FIO, Telefon, Fani, Guruhi, username, parol)
+values 	('Davron Ergashev', '+998901237898', 'Matematika', '1 - guruh', 'Davronbek', 'DavronE98'),
+		('Tohir Hamidov', '+998993227899', 'Ingliz tili', '1 - guruh', 'Tohirbek', 'TohirH99');
 
 
 insert into courses(Nomi, Davomiyligi, Oqituvchi, Narxi)
 values 	('IELTS', '4 oy', 'Tohir Hamidov', 1000000),
 		('Matematika', '12 oy', 'Davron Ergashev', 600000);
+
+
+insert into admins(username, parol)
+values 	('1', '1');       
+       
+
+insert into teachers(username, parol)
+values 	('2', '2');
 
 
 
